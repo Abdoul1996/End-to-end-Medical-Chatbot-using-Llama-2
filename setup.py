c660202a82a7710f760796289c9d496f04800c07
aabdillahi@mail.sfsu.edu <aabdillahi@mail.sfsu.edu>
2024-06-07 16:08:28 -0700

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+# code is very common 
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical chatbot',
+    version='0.0.0',
+    author='Abdoulfatah Abdillahi',
+    author_email='aabdillahid@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
